396fe6ebcc891eedb93ff9d754cc60487e34f774
Hao Li <lihaosky@hotmail.com>
2012-03-06 12:16:03 -0500

    Add class for commandtype in message

---
+++ simulator/client/ycsb/src/com/yahoo/ycsb/shard/CommandType.java
@@ -0,0 +1,30 @@
+package com.yahoo.ycsb.shard;
+
+/**
+ * Command type in messages
+ * @author lihao
+ *
+ */
+public class CommandType {
+	/**
+	 * Read from server db
+	 */
+	public static final int READ = 0;
+	/**
+	 * Insert into server db
+	 */
+	public static final int INSERT = 1;
+	/**
+	 * Update server db
+	 */
+	public static final int UPDATE = 2;
+	/**
+	 * Delete from server db
+	 */
+	public static final int DELETE = 3;
+	/**
+	 * Request mapping from controller
+	 */
+	public static final int REQUESTMAP = 4;
+	
+}
