6d09819e6748d86023d3ab771c9cd42e4cbc86d6
Hao Li <lihaosky@hotmail.com>
2012-03-12 13:41:58 -0400

    Removed key length field in data packet

--- simulator/client/ycsb/src/com/yahoo/ycsb/shard/ShardServerConnection.java
+++ simulator/client/ycsb/src/com/yahoo/ycsb/shard/ShardServerConnection.java
@@ -17,17 +17,24 @@ public class ShardServerConnection {
 	 * Data output stream for the socket 
 	 */
 	private DataOutputStream dos;
-	private int headerLength = 4 + 8 + 4 + 4 + 4 + 4;
+	/**
+	 * Total packet length:      4 bytes int
+	 * Time to arrive at server: 8 bytes long
+	 * Command type:             4 bytes int
+	 * Source ip address:        4 bytes int
+	 * Server ID:                4 bytes int 
+	 */
+	private int headerLength = 4 + 8 + 4 + 4 + 4;
 	
 	public ShardServerConnection(String hostname, int port) {
 		try {
 			s = new Socket(hostname, port);
 			dos = new DataOutputStream(s.getOutputStream());
 		} catch (UnknownHostException e) {
-			System.err.println("Unknownhost!");
+			System.err.println("Unknownhost Exception!");
 			e.printStackTrace();
 		} catch (IOException e) {
-			System.err.println("IO Exception!");
+			System.err.println("IO Exception in connecting to server!");
 			e.printStackTrace();
 		}
 	}
@@ -42,11 +49,12 @@ public class ShardServerConnection {
 	public boolean insert(long time, byte[] key, byte[] value, int serverID) {
 		int totalLength =  headerLength + key.length + value.length;
 		//Source to be done
-		writeHeader(totalLength, time, CommandType.INSERT, 0, serverID, key.length);
+		writeHeader(totalLength, time, CommandType.INSERT, 0, serverID);
 		try {
 			dos.write(key);
 			dos.write(value);
 		} catch (IOException e) {
+			System.err.println("IO Exception in insert!");
 			e.printStackTrace();
 		}
 		
@@ -62,10 +70,11 @@ public class ShardServerConnection {
 	public boolean delete(long time, byte[] key, int serverID) {
 		int totalLength = headerLength + key.length;
 		//Source to be done
-		writeHeader(totalLength, time, CommandType.DELETE, 0, serverID, key.length);
+		writeHeader(totalLength, time, CommandType.DELETE, 0, serverID);
 		try {
 			dos.write(key);
 		} catch (IOException e) {
+			System.err.println("IO Exception in delete!");
 			e.printStackTrace();
 		}
 		return true;
@@ -81,10 +90,11 @@ public class ShardServerConnection {
 	public boolean read(long time, byte[] key, byte[] value, int serverID) {
 		int totalLength = headerLength + key.length;
 		//Source to be done
-		writeHeader(totalLength, time, CommandType.READ, 0, serverID, key.length);
+		writeHeader(totalLength, time, CommandType.READ, 0, serverID);
 		try {
 			dos.write(key);
 		} catch (IOException e) {
+			System.err.println("IO Exception in read!");
 			e.printStackTrace();
 		}
 		return true;
@@ -100,26 +110,36 @@ public class ShardServerConnection {
 	public boolean update(long time, byte[] key, byte[] value, int serverID) {
 		int totalLength = headerLength + key.length + value.length;
 		//Source to be done
-		writeHeader(totalLength, time, CommandType.UPDATE, 0, serverID, key.length);
+		writeHeader(totalLength, time, CommandType.UPDATE, 0, serverID);
 		try {
 			dos.write(key);
 			dos.write(value);
 		} catch (IOException e) {
+			System.err.println("IO Exception in update!");
 			e.printStackTrace();
 		}
 		return true;
 	}
 	
-	private void writeHeader(int totalLength, long time, int command, int source, int serverID, int keyLength) {
+	private void writeHeader(int totalLength, long time, int command, int source, int serverID) {
 		try {
 			dos.writeInt(totalLength);
 			dos.writeLong(time);
 			dos.writeInt(CommandType.INSERT);
 			//Write the IP of client, to be done
 			dos.writeInt(serverID);
-			dos.writeInt(keyLength);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			System.err.println("IO Exception in writing header!");
+			e.printStackTrace();
+		}
+	}
+	
+	public void close() {
+		try {
+			dos.close();
+			s.close();
+		} catch (IOException e) {
+			System.err.println("IO Exception in closing socket!");
 			e.printStackTrace();
 		}
 	}
