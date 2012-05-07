df8177c127baebe235c8d57f0f887f9a1f6bb0dc
Hao Li <lihaosky@hotmail.com>
2012-05-07 14:07:10 -0400

    Changed driver.sh

--- simulator/client/ycsb/bin/driver.sh
+++ simulator/client/ycsb/bin/driver.sh
@@ -1,3 +1,3 @@
 echo `date +%s` >> time.txt 
-java -cp ../lib/xmemcached.jar:../lib/spymemcached-2.7.3.jar:../lib/slf4j-api-1.6.4.jar:.  com.yahoo.ycsb.memcached.MemcachedClient -p recordcount=0 -p operationcount=$1 -threads $2 -P $3 -c $4 $5
+java -cp ../lib/xmemcached.jar:../lib/spymemcached-2.7.3.jar:../lib/slf4j-api-1.6.4.jar:.  com.yahoo.ycsb.memcached.MemcachedClient -p recordcount=0 -p operationcount=$1 -threads $2 -P $3 -c $4 -policy $5 -i $6 $7 $8
 echo `date +%s` >> time.txt
